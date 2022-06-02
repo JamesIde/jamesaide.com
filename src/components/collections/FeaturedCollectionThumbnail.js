473c3be9df4bf6e496b3bc9688f30b876dbfd37b
James <james.ide775@gmail.com>
2022-06-02 19:53:51 +0930

    fix: style

--- src/components/collections/FeaturedCollectionThumbnail.js
+++ src/components/collections/FeaturedCollectionThumbnail.js
@@ -5,7 +5,7 @@ function FeaturedCollectionThumbnail({ slug, title, image, date }) {
   return (
     <div
       className="
-    container mx-auto w-1/3 mt-2"
+    container mx-auto xl:w-1/3 lg:w-1/3 md:w-1/3 mt-2 px-5"
     >
       <div className="px-6 py-2 mx-auto ">
         <div className="mb-1 mx-auto font-playfair text-[20px] text-[#343a40] leading-6 font-semibold text-center hover:text-sky-700">
