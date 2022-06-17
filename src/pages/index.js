b3db04765724d78646f2e3f0e97130ba43325e8f
James <james.ide775@gmail.com>
2022-06-17 13:56:48 +0930

    fix: index remove: quickinfo

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,12 @@ import Helmet from "../components/navigation&seo/Helmet"
 import FeaturedCollection from "../components/collections/FeaturedCollection"
 import FeaturedRecords from "../components/records/FeaturedRecords"
 import FeaturedEntries from "../components/entries/FeaturedEntries"
-import QuickInfoGuide from "../components/misc/QuickInfoGuide"
 export default function Home() {
   return (
     <>
       <Helmet title="Home" />
+      <Navigation />
       {/* <Banner /> */}
-      {/* <QuickInfoGuide /> */}
       {/* Nav goes once banner is updated after LP */}
       {/* <Navigation /> */}
       <FeaturedRecords />
