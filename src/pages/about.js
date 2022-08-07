3663f6dc9f52d8e45a62a6f49878ee9ac53c4f38
James <james.ide775@gmail.com>
2022-08-07 14:07:20 +0930

    Fix favicon

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,38 +2,8 @@ import React from "react"
 import Helmet from "../components/navigation&seo/Helmet"
 import Layout from "../components/navigation&seo/Layout"
 import Navigation from "../components/navigation&seo/Navigation"
-import { Link } from "gatsby"
-import { FaGithub } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 function About() {
-  const github = useStaticQuery(graphql`
-    query {
-      githubData {
-        data {
-          user {
-            pinnedItems {
-              nodes {
-                name
-                description
-                createdAt(formatString: "D/MM/YY")
-                url
-                repositoryTopics {
-                  edges {
-                    node {
-                      topic {
-                        name
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
-  const info = github.githubData.data.user.pinnedItems.nodes
   return (
     <>
       <Navigation />
@@ -43,11 +13,8 @@ function About() {
       <div className="xl:w-5/12 lg:w-5/12 md:w-5/12 h-40 mx-auto xl:mt-32 p-2 ">
         <h1 className="font-bold text-xl mb-3">About</h1>
         <p className="text-gray-600">
-          Hello! I'm James Ide, a web developer from Adelaide, South Australia.
-        </p>
-        <p className="text-gray-600">
-          This website is where I share hiking trips, projects, photos and
-          anything else I am interested in.
+          Developer, hiker and photographer. Sharing trips, projects, photos and
+          anything I'm interested in.
         </p>
         <h1 className="font-bold text-xl mb-3 mt-4">Contact</h1>
         <p className="text-gray-600">
@@ -75,6 +42,16 @@ function About() {
           >
             Github
           </a>
+          {""}.
+          {/* {" "}
+          or{" "}
+          <a
+            href="https://www.jameside.info/"
+            target="_#"
+            className="text-black underline"
+          >
+            See More
+          </a> */}
         </p>
       </div>
     </>
